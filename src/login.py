5ce478a6a98efa5b25c55cdf698fe13c3a944d9e
vigneshkumar1312 <vigneshkumarv1312@gmail.com>
2025-07-11 11:04:59 +0000

    sec commit

--- src/login.py
+++ src/login.py
@@ -1,26 +1,5 @@
-import json
-import boto3
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('Users')
-
 def lambda_handler(event, context):
-    data = json.loads(event['body'])
-    email = data['email']
-    password = data['password']
-
-    response = table.scan(
-        FilterExpression="email = :e and password = :p",
-        ExpressionAttributeValues={':e': email, ':p': password}
-    )
-
-    if response['Items']:
-        return {
-            'statusCode': 200,
-            'body': json.dumps({'message': 'Login successful'})
-        }
-    else:
-        return {
-            'statusCode': 401,
-            'body': json.dumps({'message': 'Invalid credentials'})
-        }
+    return {
+        "statusCode": 200,
+        "body": "Login successful!"
+    }
--- src/signup.py
+++ src/signup.py
@@ -1,22 +1,5 @@
-import json
-import boto3
-import uuid
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('test-vk-users')
-
 def lambda_handler(event, context):
-    data = json.loads(event['body'])
-    email = data['email']
-    password = data['password']
-
-    table.put_item(Item={
-        'userId': str(uuid.uuid4()),
-        'email': email,
-        'password': password  # Hash it in real apps!
-    })
-
     return {
-        'statusCode': 200,
-        'body': json.dumps({'message': 'User signed up successfully'})
+        "statusCode": 200,
+        "body": "Signup successful!"
     }
